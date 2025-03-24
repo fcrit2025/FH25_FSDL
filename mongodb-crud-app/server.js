094c111e734344ad8dc2bd3291602b23552bdb3f
fcrit2025 <pythonfh421@gmail.com>
2025-03-24 06:21:59 +0000

    cia_1

---
+++ mongodb-crud-app/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const userRoutes = require('./routes/userRoutes');
+
+const app = express();
+const PORT = 3000;
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect('mongodb+srv://avp234:<2q4OeypKQZOZp3o4>@cluster0.fk3ty.mongodb.net/User?retryWrites=true&w=majority&appName=Cluster0', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Error connecting to MongoDB:', err));
+
+// Use user routes
+app.use('/users', userRoutes);
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
